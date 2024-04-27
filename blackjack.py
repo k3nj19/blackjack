b29288c6178f88602db42b100521333831142310
curtis <62461516+hofung614@users.noreply.github.com>
2024-04-27 22:06:21 +0800

    Add files via upload

---
+++ blackjack.py
@@ -0,0 +1,79 @@
+import random
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def blackjack():
+   def process():
+       if user_total == 21:
+           print('You win.')
+           cont = False
+       elif computer_total == 21:
+           print('Computer wins.')
+           cont = False
+       elif user_total and computer_total == 21:
+           print('Draw.')
+           cont = False
+       elif user_total and computer_total != 21:
+           if user_total > 21:
+               print('You went over. You lose.')
+               cont = False
+           elif computer_total > 21:
+               print('Computer went over. You win.')
+               cont = False
+           else:
+               if new_card == 'y':
+                   pass
+                   cont = True
+               else:
+                   if user_total > computer_total:
+                       print('You win.')
+                       cont = False
+                   elif user_total < computer_total:
+                       print('You lose.')
+                       cont = False
+                   else:
+                       print('Draw.')
+                       cont = False
+
+   user_cards = []
+   computer_cards = []
+
+   for n in range(2):
+       user_cards.append(random.choice(cards))
+       computer_cards.append(random.choice(cards))
+       user_total = sum(user_cards)
+       computer_total = sum(computer_cards)
+   print(f'Your cards: {user_cards}, current score: {user_total}')
+   print(f"Computer's first draw: {computer_cards[0]}")
+   new_card = input("Type 'y' to get another card, type 'n' to pass.").lower()
+   if new_card == 'y':
+       cont = True
+       process()
+   else:
+       cont = False
+       print(f'Your final hand: {user_cards}, final score: {user_total}')
+       print(f"Computer's final hand: {computer_cards}, final score: {computer_total}")
+       process()
+
+   while cont:
+       user_cards.append(random.choice(cards))
+       computer_cards.append(random.choice(cards))
+       user_total = sum(user_cards)
+       computer_total = sum(computer_cards)
+       print(f'Your cards: {user_cards}, current score: {user_total}')
+       print(f"Computer's first draw: {computer_cards[0]}")
+       new_card = input("Type 'y' to get another card, type 'n' to pass. ").lower()
+       if new_card == 'y':
+           cont = True
+           process()
+       else:
+           cont = False
+           print(f'Your final hand: {user_cards}, final score: {user_total}')
+           print(f"Computer's final hand: {computer_cards}, final score: {computer_total}")
+           process()
+   play_again = input("Type 'y' to play another game, type 'n' to stop. ").lower()
+   if play_again == 'y':
+       blackjack()
+   else:
+       print('Goodbye.')
+blackjack()
+
